2ed4c2207742bfd12a14a8173abf7058016c82df
narendra-2605 <narendra.vema@ieee.org>
2022-01-07 01:41:02 +0530

    Updated Home page and added login and signup page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgImageSliderModule } from 'ng-image-slider';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,11 +33,15 @@ import { FooterComponent } from './footer/footer.component';
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
+    NgImageSliderModule,
     RouterModule.forRoot([
       { path: 'home', component: HomeComponent },
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'header', component: HeaderComponent },
-      { path: 'footer', component: FooterComponent }
+      { path: 'footer', component: FooterComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent }
+
     ])
   ],
   providers: [],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgImageSliderModule } from 'ng-image-slider';
+
 
 @Component({
   selector: 'app-home',
@@ -11,5 +13,25 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  imageObject: Array<object> = [{
+    // image: 'assets/img/slider/1.jpg',
+    image: '../../assets/juit.jpg',
+    thumbImage: '../../ assets/juit.jpg',
+    alt: 'Juit of image',
+    title: 'JUIT image'
+  }, {
+    image: '../../assets/juit1.jpg', // Support base64 image
+    thumbImage: '../../assets/juit1.jpg', // Support base64 image
+    title: 'Image title', //Optional: You can use this key if want to show image with title
+    alt: 'JUIT 1 image', //Optional: You can use this key if want to show image with alt
+    order: 1 //Optional: if you pass this key then slider images will be arrange according @input: slideOrderType
+  }, {
+    image: '../../assets/juit2.jpg',
+    thumbImage: '../../assets/juit2.jpg',
+    alt: 'JUIT 2 image',
+    title: 'title of image'
+  }
+  ];
 
 }
